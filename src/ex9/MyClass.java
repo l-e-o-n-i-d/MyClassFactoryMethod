13cad75fa7be98412902892ae352bc1a3e3914c6
l-e-o-n-i-d <diskoboy777@gmail.com>
2023-06-23 23:44:37 +0300

    remove overloaded factoryMethod and constructors

--- src/ex9/MyClass.java
+++ src/ex9/MyClass.java
@@ -2,35 +2,20 @@ package ex9;
 
 public class MyClass<T> {
     private T value;
-    private MyClass(T val){
-        this.value =val;
-    }
-    private MyClass(){
-    }
-
     public T getValue() {
         return value;
     }
-
     public void setValue(T value) {
         this.value = value;
     }
-
     public static <T> MyClass<T> factoryMethod(){
         return new MyClass<>();
     }
-
-    public static <T> MyClass<T> factoryMethod(T value){
-        return new MyClass<>(value);
-    }
 }
-
 class Main{
     public static void main(String[] args) {
         MyClass<String> stringMyClass = MyClass.factoryMethod();
         stringMyClass.setValue("String value");
         System.out.println(stringMyClass.getValue());
-        MyClass<Integer> integerMyClass = MyClass.factoryMethod(12);
-        System.out.println(integerMyClass.getValue());
     }
 }
